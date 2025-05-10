8313885a963671a7dd1ca66eb1bdb62c2237d944
SeungKang <13smk@queensu.ca>
2025-05-09 23:11:16 -0400

    memory: create pointers from strings

--- internal/memory/memory.go
+++ internal/memory/memory.go
@@ -1,7 +1,38 @@
 package memory
 
+import (
+	"strconv"
+	"strings"
+)
+
 type Pointer struct {
 	Name      string
 	Addrs     []uintptr
 	OptModule string
+}
+
+func CreatePointerFromString(ptrDefinition string) (Pointer, error) {
+	var ptr Pointer
+	var ptrChain string
+
+	before, after, found := strings.Cut(ptrDefinition, ":")
+	if found {
+		ptr.OptModule = before
+		ptrChain = after
+	} else {
+		ptrChain = before
+	}
+
+	ptrParts := strings.Split(ptrChain, ",")
+	ptr.Addrs = make([]uintptr, len(ptrParts))
+	for i, part := range ptrParts {
+		addr, err := strconv.ParseUint(strings.TrimPrefix(part, "0x"), 16, 64)
+		if err != nil {
+			return Pointer{}, err
+		}
+
+		ptr.Addrs[i] = uintptr(addr)
+	}
+
+	return ptr, nil
 }

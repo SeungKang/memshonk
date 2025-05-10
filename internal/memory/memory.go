a9028834f91820e40fed29216e6988d7c1e0bc6d
Stephen Fox <stephen.j.fox.jr@gmail.com>
2025-05-10 19:49:04 +0000

    memory: Added String method to Pointer

--- internal/memory/memory.go
+++ internal/memory/memory.go
@@ -5,10 +5,77 @@ import (
 	"strings"
 )
 
+const (
+	addrSep = ","
+)
+
 type Pointer struct {
 	Name      string
 	Addrs     []uintptr
 	OptModule string
+}
+
+func (o Pointer) Advance(by uint64) Pointer {
+	if by == 0 {
+		return o
+	}
+
+	lastIndex := len(o.Addrs) - 1
+
+	if lastIndex < 0 {
+		return o
+	}
+
+	last := o.Addrs[lastIndex]
+
+	o.Addrs[lastIndex] = last + uintptr(by)
+
+	return o
+}
+
+func (o Pointer) Offset(by int64) Pointer {
+	if by == 0 {
+		return o
+	}
+
+	lastIndex := len(o.Addrs) - 1
+
+	if lastIndex < 0 {
+		return o
+	}
+
+	last := o.Addrs[lastIndex]
+
+	if by < 0 {
+		u := uintptr((by * -1))
+		o.Addrs[lastIndex] = last - u
+	} else {
+		o.Addrs[lastIndex] = last + uintptr(by)
+	}
+
+	return o
+}
+
+func (o Pointer) String() string {
+	var buf string
+
+	if o.Name != "" {
+		buf += o.Name + " "
+	}
+
+	if o.OptModule != "" {
+		buf += o.OptModule + ":"
+	}
+
+	for i, addr := range o.Addrs {
+		buf += "0x" + strconv.FormatUint(uint64(addr), 16)
+
+		if i != len(o.Addrs)-1 {
+			buf += addrSep
+		}
+	}
+
+	return buf
 }
 
 func CreatePointerFromString(ptrDefinition string) (Pointer, error) {
@@ -23,7 +90,7 @@ func CreatePointerFromString(ptrDefinition string) (Pointer, error) {
 		ptrChain = before
 	}
 
-	ptrParts := strings.Split(ptrChain, ",")
+	ptrParts := strings.Split(ptrChain, addrSep)
 	ptr.Addrs = make([]uintptr, len(ptrParts))
 	for i, part := range ptrParts {
 		addr, err := strconv.ParseUint(strings.TrimPrefix(part, "0x"), 16, 64)

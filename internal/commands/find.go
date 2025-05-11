7d6adf1e05a029bf0396b4b31eac6e25e6f34c76
SeungKang <13smk@queensu.ca>
2025-05-11 19:25:56 -0400

    find: find command

---
+++ internal/commands/find.go
@@ -0,0 +1,47 @@
+package commands
+
+import (
+	"context"
+	"fmt"
+	"github.com/SeungKang/memshonk/internal/memory"
+)
+
+var _ Command = (*FindCommand)(nil)
+
+type FindCommandArgs struct {
+	Pattern   string
+	StartAddr string
+}
+
+func NewFindCommand(args FindCommandArgs) FindCommand {
+	return FindCommand{
+		args: args,
+	}
+}
+
+type FindCommand struct {
+	args FindCommandArgs
+}
+
+func (o FindCommand) Run(ctx context.Context, inOut IO, s Session) error {
+	addrStr := o.args.StartAddr
+
+	var ptr memory.Pointer
+	ptr, err := memory.CreatePointerFromString(addrStr)
+	if err != nil {
+		return err
+	}
+
+	reader := memory.NewBufferedReader(s.Process(), ptr)
+	matches, err := memory.FindAllReader(o.args.Pattern, reader)
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprintln(inOut.Stdout, "matches:", matches[0])
+	if err != nil {
+		return err
+	}
+
+	return nil
+}

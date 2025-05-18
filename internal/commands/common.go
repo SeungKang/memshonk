e325accb37f8079b77997637a4507cd81fc3340c
Stephen Fox <stephen.j.fox.jr@gmail.com>
2025-05-18 19:00:35 +0000

    commands: Initial work on simplifying definitions

--- internal/commands/common.go
+++ internal/commands/common.go
@@ -8,6 +8,54 @@ import (
 	"github.com/SeungKang/memshonk/internal/progctl"
 )
 
+func BuiltinCommands() []CommandSchema {
+	return []CommandSchema{
+		PluginsCommandSchema(),
+	}
+}
+
+type CommandSchema struct {
+	Name      string
+	Aliases   []string
+	ShortHelp string
+	LongHelp  string
+	NonFlags  []NonFlagSchema
+	Flags     []FlagSchema
+	CreateFn  func(CommandConfig) (Command, error)
+}
+
+type FlagSchema struct {
+	Short       string
+	Long        string
+	Description string
+	DataType    interface{}
+	DefaultVal  interface{}
+}
+
+type NonFlagSchema struct {
+	Name     string
+	Desc     string
+	DataType interface{}
+	DefValue interface{}
+}
+
+type ArgDataTypeSchema struct {
+	Type interface{}
+}
+
+type CommandConfig struct {
+	NonFlags ArgFetcher
+	Flags    ArgFetcher
+}
+
+type ArgFetcher interface {
+	String(argID string) string
+	Int(argID string) int
+	Int64(argID string) int64
+	Uint(argID string) uint
+	Uint64(argID string) uint64
+}
+
 type Command interface {
 	Run(context.Context, IO, Session) error
 }

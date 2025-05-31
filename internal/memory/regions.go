f764291a66bd3f54b1443c6cc418f17ef64e7652
Stephen Fox <stephen.j.fox.jr@gmail.com>
2025-05-31 00:44:23 +0000

    memory: Initial work towards merging objects

--- internal/memory/regions.go
+++ internal/memory/regions.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"fmt"
 	"sort"
+	"strconv"
+	"strings"
 )
 
 const (
@@ -79,10 +81,117 @@ func (o MemoryState) Letter() byte {
 
 type Regions struct {
 	regions []Region
+	objects []Object
+	nonobjs []*Region
+}
+
+type Object struct {
+	ID       ObjectID
+	BaseAddr uintptr
+	EndAddr  uintptr
+	Name     string
+	Path     string
+	Regions  []*Region
+}
+
+func (o Object) Matches(str string) bool {
+	if len(o.Regions) == 0 {
+		return false
+	}
+
+	return strings.Contains(o.Regions[0].Parent.FileName, str) ||
+		strings.Contains(o.Regions[0].Parent.FilePath, str)
+}
+
+func (o Object) String() string {
+	buf := bytes.Buffer{}
+
+	if len(o.Regions) == 0 {
+		buf.WriteString(o.ID.String())
+
+		buf.WriteString(" <empty-object>")
+
+		return buf.String()
+	}
+
+	buf.WriteString(o.Regions[0].NameOrPath())
+
+	buf.WriteString(" (id: ")
+
+	buf.WriteString(o.ID.String())
+
+	buf.WriteString(")")
+
+	for _, region := range o.Regions {
+		buf.WriteString("\n|-- ")
+
+		buf.WriteString(region.StringWithoutObject())
+	}
+
+	return buf.String()
+}
+
+type ObjectMeta struct {
+	IsSet    bool
+	ID       ObjectID
+	FilePath string
+	FileName string
+}
+
+type ObjectID uint64
+
+func (o ObjectID) String() string {
+	return strconv.FormatUint(uint64(o), 10)
 }
 
 func (o *Regions) Add(region Region) {
 	o.regions = append(o.regions, region)
+
+	if region.Parent.IsSet {
+		index, hasIt := o.objectIndex(region.Parent.ID)
+		if hasIt {
+			obj := &o.objects[index]
+			obj.Regions = append(obj.Regions, &region)
+
+			if region.BaseAddr < obj.BaseAddr {
+				obj.BaseAddr = region.BaseAddr
+			}
+
+			if region.EndAddr > obj.EndAddr {
+				obj.EndAddr = region.EndAddr
+			}
+		} else {
+			o.objects = append(o.objects, Object{
+				ID:       region.Parent.ID,
+				BaseAddr: region.BaseAddr,
+				EndAddr:  region.EndAddr,
+				Name:     region.Parent.FileName,
+				Path:     region.Parent.FilePath,
+				Regions:  []*Region{&region},
+			})
+		}
+	} else {
+		o.nonobjs = append(o.nonobjs, &region)
+	}
+}
+
+func (o *Regions) ObjectByID(id ObjectID) (Object, bool) {
+	index, hasIt := o.objectIndex(id)
+	if hasIt {
+		return o.objects[index], true
+	}
+
+	return Object{}, false
+}
+
+func (o *Regions) objectIndex(id ObjectID) (int, bool) {
+	for i, obj := range o.objects {
+		if obj.ID == id {
+			return i, true
+		}
+	}
+
+	return 0, false
 }
 
 func (o *Regions) Iter(fn func(i int, region Region) error) error {
@@ -102,6 +211,14 @@ func (o *Regions) Iter(fn func(i int, region Region) error) error {
 
 func (o *Regions) Len() int {
 	return len(o.regions)
+}
+
+func (o *Regions) ObjectsLen() int {
+	return len(o.objects)
+}
+
+func (o *Regions) NonObjectsLen() int {
+	return len(o.nonobjs)
 }
 
 func (o *Regions) Less(i, j int) bool {
@@ -136,6 +253,67 @@ func (o *Regions) HasAddr(addr uintptr) (*Region, bool) {
 	return nil, false
 }
 
+func (o *Regions) FirstObjectMatching(str string) (Object, error) {
+	var match Object
+	var found bool
+
+	err := o.IterObjectsMatching(str, func(obj Object) error {
+		found = true
+		match = obj
+
+		return ErrStopIterating
+	})
+	if err != nil {
+		return Object{}, err
+	}
+
+	if !found {
+		return Object{}, fmt.Errorf("failed to find a match for: %q", str)
+	}
+
+	return match, nil
+}
+
+func (o *Regions) IterObjectsMatching(str string, fn func(Object) error) error {
+	return o.IterObjects(func(obj Object) error {
+		if obj.Matches(str) {
+			return fn(obj)
+		}
+
+		return nil
+	})
+}
+
+func (o *Regions) IterObjects(fn func(Object) error) error {
+	for _, object := range o.objects {
+		err := fn(object)
+		if err != nil {
+			if errors.Is(err, ErrStopIterating) {
+				return nil
+			}
+
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (o *Regions) IterNonObjects(fn func(*Region) error) error {
+	for _, region := range o.nonobjs {
+		err := fn(region)
+		if err != nil {
+			if errors.Is(err, ErrStopIterating) {
+				return nil
+			}
+
+			return err
+		}
+	}
+
+	return nil
+}
+
 type Region struct {
 	BaseAddr  uintptr
 	EndAddr   uintptr
@@ -148,13 +326,31 @@ type Region struct {
 	Writeable  bool
 	Executable bool
 	Copyable   bool
+
+	Parent ObjectMeta
 }
 
-func (o Region) Unaccessible() bool {
+func (o Region) NoPermissions() bool {
 	return !o.Readable && !o.Writeable && !o.Executable
 }
 
 func (o Region) String() string {
+	buf := bytes.Buffer{}
+
+	buf.WriteString(o.StringWithoutObject())
+
+	if o.Parent.IsSet {
+		buf.WriteByte(' ')
+
+		buf.WriteString(o.NameOrPath())
+
+		buf.WriteString(" (id: " + o.Parent.ID.String() + ")")
+	}
+
+	return buf.String()
+}
+
+func (o Region) StringWithoutObject() string {
 	buf := bytes.Buffer{}
 
 	buf.WriteString(fmt.Sprintf("%#012x-%#012x (allocb: %#012x) ",
@@ -194,4 +390,15 @@ func (o Region) String() string {
 		o.State.String()))
 
 	return buf.String()
+}
+
+func (o Region) NameOrPath() string {
+	switch {
+	case o.Parent.FilePath != "":
+		return o.Parent.FilePath
+	case o.Parent.FileName != "":
+		return o.Parent.FileName
+	default:
+		return "<no-name>"
+	}
 }

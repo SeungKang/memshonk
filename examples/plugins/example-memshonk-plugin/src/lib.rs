463747b1d7d8dff8875418d8bddf5a1ac53edbca
SeungKang <13smk@queensu.ca>
2025-07-17 21:21:43 -0400

    plugin: plugin command argument parser

--- examples/plugins/example-memshonk-plugin/src/lib.rs
+++ examples/plugins/example-memshonk-plugin/src/lib.rs
@@ -1,8 +1,11 @@
 use core::ptr;
 
+use argparse::{Store, StoreTrue};
 use mskit::{ShareableType, SharedBufRef};
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::error::Error;
+use std::str::FromStr;
 
 #[no_mangle]
 extern "C" fn version() -> u16 {
@@ -85,6 +88,96 @@ extern "C" fn example_command(args: *mut u8, output_ptr: *mut *mut u8) -> *mut u
     }
 }
 
-fn _example_command(args: Vec<String>) -> Result<String, Box<dyn Error>> {
-    Ok(args.join(" "))
+fn _example_command(mut arg_list: Vec<String>) -> Result<String, Box<dyn Error>> {
+    let mut temp: Vec<String> = Vec::new();
+    temp.push("".into());
+    arg_list.append(&mut temp);
+
+    let mut args = ExampleCommandArgs {
+        addr: HexAddr(0),
+        value: HexValue(Vec::new()),
+    };
+
+    let mut parser = argparse::ArgumentParser::new();
+    parser
+        .refer(&mut args.addr)
+        .add_option(&["--addr"], Store, "the address to write to");
+
+    parser
+        .refer(&mut args.value)
+        .add_option(&["--value"], Store, "the value to write");
+
+    let mut stdout = Vec::new();
+    let mut stderr = Vec::new();
+    if parser.parse(arg_list, &mut stdout, &mut stderr).is_err() {
+        if stdout.is_empty() {
+            return Err(format!("{}", String::from_utf8(stderr).unwrap()))?;
+        }
+
+        return Ok(String::from_utf8(stdout)?);
+    }
+
+    drop(parser);
+
+    mskit::write_to_process(args.addr.0, args.value.0)?;
+
+    Ok("".into())
+}
+
+struct ExampleCommandArgs {
+    addr: HexAddr,
+    value: HexValue,
+}
+
+struct HexAddr(usize);
+impl FromStr for HexAddr {
+    type Err = Box<dyn Error>;
+
+    fn from_str(mut s: &str) -> Result<Self, Self::Err> {
+        s = s.strip_prefix("0x").unwrap_or(s);
+
+        let value = u64::from_str_radix(s, 16)?;
+        Ok(HexAddr(value as usize))
+    }
+}
+
+impl Clone for HexAddr {
+    fn clone(&self) -> Self {
+        HexAddr(self.0.clone())
+    }
+}
+
+struct HexValue(Vec<u8>);
+impl FromStr for HexValue {
+    type Err = Box<dyn Error>;
+
+    fn from_str(mut s: &str) -> Result<Self, Self::Err> {
+        s = s.strip_prefix("0x").unwrap_or(s);
+        let bytes = if s.len() % 2 != 0 {
+            let mut owned = String::new();
+            owned.push('0');
+            owned.push_str(s);
+            hex_to_bytes(&owned)
+        } else {
+            hex_to_bytes(s)
+        };
+
+        Ok(HexValue(bytes.unwrap_or(Vec::new())))
+    }
+}
+
+impl Clone for HexValue {
+    fn clone(&self) -> Self {
+        HexValue(self.0.clone())
+    }
+}
+
+fn hex_to_bytes(mut s: &str) -> Option<Vec<u8>> {
+    (0..s.len())
+        .step_by(2)
+        .map(|i| {
+            s.get(i..i + 2)
+                .and_then(|sub| u8::from_str_radix(sub, 16).ok())
+        })
+        .collect()
 }

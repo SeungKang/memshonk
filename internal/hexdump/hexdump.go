a3ca149f6d004895e9c061bcb9a43c3792d27543
Stephen Fox <stephen.j.fox.jr@gmail.com>
2025-07-24 23:45:05 +0000

    hexdump: Refactor + support custom row length

--- internal/hexdump/hexdump.go
+++ internal/hexdump/hexdump.go
@@ -14,6 +14,8 @@ type Config struct {
 	Src    io.Reader
 	Dst    io.Writer
 	Colors Colors
+
+	OptRowLen uint16
 }
 
 func Dump(ctx context.Context, config Config) error {
@@ -21,43 +23,67 @@ func Dump(ctx context.Context, config Config) error {
 	bufW := bufio.NewWriter(config.Dst)
 	defer bufW.Flush()
 
-	offset := int64(-1) // TODO
-	row := make([]byte, 16)
+	maxRowLen := config.OptRowLen
+	if maxRowLen == 0 {
+		maxRowLen = 16
+	}
+
+	rowArgs := dumpRowArgs{
+		writer:    bufW,
+		row:       make([]byte, maxRowLen),
+		maxRowLen: maxRowLen,
+		colors:    config.Colors,
+	}
 
 	for {
-		b, err := bufR.ReadByte()
+		n, err := bufR.Read(rowArgs.row)
+
+		if n > 0 {
+			rowArgs.offset += uint64(n) - 1
+			rowArgs.rowOffset = uint16(n) - 1
+
+			dErr := dumpRow(rowArgs)
+			if dErr != nil {
+				return dErr
+			}
+		}
+
 		if err != nil {
 			if errors.Is(err, io.EOF) {
-				return dumpRow(bufW, offset, row, config.Colors, false)
+				return nil
 			}
 
 			return err
 		}
-
-		offset++
-		rowOffset := offset % 16
-		row[rowOffset] = b
-
-		if offset > 0 && rowOffset == 15 {
-			err := dumpRow(bufW, offset, row, config.Colors, true)
-			if err != nil {
-				return err
-			}
-		}
 	}
 }
 
-func dumpRow(w io.Writer, offset int64, row []byte, colors Colors, addNewline bool) error {
-	rowLen := int(offset%16 + 1)
+type dumpRowArgs struct {
+	writer    io.Writer
+	maxRowLen uint16
+	colors    Colors
+	offset    uint64
+	row       []byte
+	rowOffset uint16
+}
 
-	s := fmt.Sprintf("%08x", offset-int64(rowLen)+1) + "   "
+func dumpRow(args dumpRowArgs) error {
+	rowLen := args.rowOffset + 1
 
-	for i := 0; i < 16; i++ {
+	var s string
+	if args.offset > uint64(args.maxRowLen) {
+		s = "\n"
+	}
+
+	s += fmt.Sprintf("%08x", args.offset-uint64(args.rowOffset)) + "   "
+
+	for i := uint16(0); i < args.maxRowLen; i++ {
 		if i < rowLen {
-			s += colors.HexChar(row[i]) + " "
+			s += args.colors.HexChar(args.row[i]) + " "
 		} else {
 			s += "   "
 		}
+
 		if (i+1)%4 == 0 {
 			s += " "
 		}
@@ -65,27 +91,24 @@ func dumpRow(w io.Writer, offset int64, row []byte, colors Colors, addNewline bo
 
 	s += " |"
 
-	for i := 0; i < 16; i++ {
+	for i := uint16(0); i < args.maxRowLen; i++ {
 		if i < rowLen {
 			b := byte('.')
 
-			if row[i] >= 33 && row[i] <= 126 {
-				b = row[i]
+			if args.row[i] >= 0x21 && args.row[i] <= 0x7e {
+				// Is ASCII (except space).
+				b = args.row[i]
 			}
 
-			s += colors.Value(string(b), row[i])
+			s += args.colors.Value(string(b), args.row[i])
 		} else {
 			s += " "
 		}
 	}
 
-	if addNewline {
-		s += "|\n"
-	} else {
-		s += "|"
-	}
+	s += "|"
 
-	_, err := fmt.Fprint(w, s)
+	_, err := fmt.Fprint(args.writer, s)
 
 	return err
 }
